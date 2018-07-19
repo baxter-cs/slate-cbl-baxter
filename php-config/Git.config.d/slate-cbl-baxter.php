025472c98d3f80c5f209596843335c3a7d52b19d
Hallie Larsson <hallie.larsson@baxter-academy.org>
2018-07-19 16:48:45 +0000

    Added a test file

---
+++ php-config/Git.config.d/slate-cbl-baxter.php
@@ -0,0 +1,12 @@
+<?php
+
+Git::$repositories['slate-cbl-baxter'] = [
+    'remote' => 'git@github.com:baxter-cs/slate-cbl-baxter.git',
+    'originBranch' => 'master',
+    'workingBranch' => 'master',
+    'trees' => [
+        'html-templates',
+        'site-root',
+        'php-config',
+    ]
+];
