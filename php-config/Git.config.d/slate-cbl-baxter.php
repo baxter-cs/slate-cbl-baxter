33f5166ee5c478181eb3c78f8416324611cbcbc7
Chris Alfano <chris@jarv.us>
2018-08-24 22:27:43 +0000

    fix: narrow Git config paths

--- php-config/Git.config.d/slate-cbl-baxter.php
+++ php-config/Git.config.d/slate-cbl-baxter.php
@@ -2,12 +2,14 @@
 
 Git::$repositories['slate-cbl-baxter'] = [
     'remote' => 'git@github.com:baxter-cs/slate-cbl-baxter.git',
-    'originBranch' => 'master',
-    'workingBranch' => 'master',
+    'originBranch' => 'builds/v1',
+    'workingBranch' => 'builds/v1',
     'trees' => [
-        'html-templates',
-        'site-root',
-        'php-config',
-        'php-classes'
+        'php-classes/Slate/CBL/StudentCompetency.php',
+        'php-config/Git.config.d/slate-cbl-baxter.php',
+        'php-config/Slate/CBL',
+        'sencha-workspace/packages/slate-cbl-baxxter',
+        'site-root/baxter'
     ]
 ];
+
