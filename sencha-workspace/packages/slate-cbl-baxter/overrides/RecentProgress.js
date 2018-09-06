9c03670d30235c3a0c081f70ddd8d986898b9c9c
halstedlarsson <halsted.larsson@gmail.com>
2018-09-05 23:32:50 -0400

    Disabled Recent Progress View

---
+++ sencha-workspace/packages/slate-cbl-baxter/overrides/RecentProgress.js
@@ -0,0 +1,12 @@
+Ext.define('Baxter.cbl.overrides.RecentProgress', {
+    override:'SlateDemonstrationsStudent.view.RecentProgress',
+
+    title: 'Recent Progress',
+    cls: 'slate-tasks-student-recentactivity',
+    tpl: [
+        '<div class="table-ct">',
+            '<div>Temporarily Disabled</div>',
+        '</div>'
+    ],
+
+});
