14ec9d381795761088b723877041b5bcd8d30207
Chris Alfano <chris@jarv.us>
2018-11-09 22:01:47 +0000

    refactor: remove StudentCompetency override

--- php-config/Git.config.d/slate-cbl-baxter.php
+++ php-config/Git.config.d/slate-cbl-baxter.php
@@ -5,7 +5,6 @@ Git::$repositories['slate-cbl-baxter'] = [
     'originBranch' => 'builds/v1',
     'workingBranch' => 'builds/v1',
     'trees' => [
-        'php-classes/Slate/CBL/StudentCompetency.php',
         'php-config/Emergence/WebApps/SenchaApp.config.d/slate-cbl-baxter.php',
         'php-config/Git.config.d/slate-cbl-baxter.php',
         'php-config/Site.config.d/slate-cbl-baxter.php',
