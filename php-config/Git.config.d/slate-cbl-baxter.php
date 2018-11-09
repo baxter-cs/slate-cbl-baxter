98ff802072cd93c81544396732515dfc8ab493b7
Chris Alfano <chris@jarv.us>
2018-11-09 22:53:55 +0000

    fix: remove derelict Site.config.d file from git mappings

--- php-config/Git.config.d/slate-cbl-baxter.php
+++ php-config/Git.config.d/slate-cbl-baxter.php
@@ -7,7 +7,6 @@ Git::$repositories['slate-cbl-baxter'] = [
     'trees' => [
         'php-config/Emergence/WebApps/SenchaApp.config.d/slate-cbl-baxter.php',
         'php-config/Git.config.d/slate-cbl-baxter.php',
-        'php-config/Site.config.d/slate-cbl-baxter.php',
         'php-config/Slate/UI/UserProfile.config.d/cbl-baxter.php',
         'php-config/Slate/CBL/Demonstrations/DemonstrationSkill.config.d/baxter.php',
         'php-config/Slate/CBL/StudentCompetency.config.d/demonstration-conditions.php',
