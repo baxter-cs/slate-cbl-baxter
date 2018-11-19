8e9d0b98d913190f0ef6a00453fcabaf063f1698
Chris Alfano <chris@jarv.us>
2018-11-18 17:55:18 -0800

    refactor: remove student progress dashboard legend from override
    It is part of the standard dashboard now

--- sencha-workspace/packages/slate-cbl-baxter/overrides/StudentDemonstrationsDashboard.js
+++ sencha-workspace/packages/slate-cbl-baxter/overrides/StudentDemonstrationsDashboard.js
@@ -9,24 +9,5 @@ Ext.define('Baxter.cbl.overrides.StudentDemonstrationsDashboard', {
 
         me.remove(me.getCompetenciesSummary(), false);
         me.remove(me.getRecentProgress(), false);
-
-        me.insert(0, {
-            xtype: 'component',
-            style: {
-                textAlign: 'right',
-                marginBottom: '3%'
-            },
-            renderTpl: [
-                '<div class="cbl-grid-legend">',
-                    '<span class="cbl-grid-legend-label">Portfolios:&ensp;</span>',
-                    '<tpl foreach="Slate.cbl.util.Config.getLevels()">',
-                        '<tpl if="xkey != 0">',
-                            '<span class="cbl-grid-legend-item level-color cbl-level-{$}" title="{title:htmlEncode}">{abbreviation:htmlEncode}</span>',
-                        '</tpl>',
-                    '</tpl>',
-                '</div>'
-            ]
-        });
     }
 });
-
