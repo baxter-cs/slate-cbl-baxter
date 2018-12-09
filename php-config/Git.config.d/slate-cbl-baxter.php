59b2f964a5437df5f8bebd9c1015c19c8cafb42c
Chris Alfano <chris@jarv.us>
2018-12-09 09:28:00 +0000

    chore: add new StudentCompetency default configs

--- php-config/Git.config.d/slate-cbl-baxter.php
+++ php-config/Git.config.d/slate-cbl-baxter.php
@@ -9,9 +9,7 @@ Git::$repositories['slate-cbl-baxter'] = [
         'php-config/Git.config.d/slate-cbl-baxter.php',
         'php-config/Slate/UI/UserProfile.config.d/cbl-baxter.php',
         'php-config/Slate/CBL/Demonstrations/DemonstrationSkill.config.d/baxter.php',
-        'php-config/Slate/CBL/StudentCompetency.config.d/demonstration-conditions.php',
-        'php-config/Slate/CBL/StudentCompetency.config.d/demonstrations-at-level.php',
-        'php-config/Slate/CBL/StudentCompetency.config.d/is-level-complete.php',
+        'php-config/Slate/CBL/StudentCompetency.config.d',
         'sencha-workspace/packages/slate-cbl-baxter',
         'site-root/baxter',
         'html-templates/baxter'
